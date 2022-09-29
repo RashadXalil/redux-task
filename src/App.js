29939affb5275a24ac493a72a3763862b464d40d
RashadXalil <rashadmkh@code.edu.az>
2022-09-29 05:25:51 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Provider } from 'react-redux'
+import { Routes, Route } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Reducers } from './redux/reducers/reducers'
+import Customers from './pages/Customers/Customers'
+import Favorites from './pages/Favorites/Favorites'
+import Home from './pages/Home/Home'
+const customerStore = createStore(Reducers)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={customerStore}>
+      <Home />
+      <Routes>
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
